3d071573b91b1e4960b549722b3c99a6eab833c1
JacksonDeLima <jacksonluis444@gmail.com>
2026-03-24 18:24:13 -0300

    MVP Taskflow

---
+++ src/test/java/com/jacksondelima/taskflow/TaskflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacksondelima.taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

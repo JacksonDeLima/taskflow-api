d6dd20d10c5b82a703860abaaa8807624680c0cd
JacksonDeLima <jacksonluis444@gmail.com>
2026-03-24 18:33:29 -0300

    Adicionar enums Role e TaskStatus

---
+++ src/main/java/com/jacksondelima/taskflow/enums/Role.java
@@ -0,0 +1,6 @@
+package com.jacksondelima.taskflow.enums;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ src/main/java/com/jacksondelima/taskflow/enums/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.jacksondelima.taskflow.enums;
+
+public enum TaskStatus {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED
+}

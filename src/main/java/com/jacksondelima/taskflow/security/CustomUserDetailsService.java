dcf2a8a3fbb064dbed1cc915babaf81c292c3596
JacksonDeLima <jacksonluis444@gmail.com>
2026-03-24 19:15:23 -0300

    Adicionado serviço CustomUserDetailsService para carregamento de detalhes do usuário

---
+++ src/main/java/com/jacksondelima/taskflow/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.jacksondelima.taskflow.security;
+
+import com.jacksondelima.taskflow.entity.User;
+import com.jacksondelima.taskflow.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
+}
